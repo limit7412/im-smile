97f0a9a3345d7e4dfe56ab475f7a8badf2452c6c
kairox <git@newgenerations.im>
2018-12-23 16:02:43 +0900

    証明書を明示的指定してやる

--- src/runtime/webhook.cr
+++ src/runtime/webhook.cr
@@ -10,7 +10,7 @@ class WebHook
   def post(body)
     ssl = OpenSSL::SSL::Context::Client.new
     # Lambda上で明示的にクライアント証明書を示す必要がある
-    ssl.ca_certificates = "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem"
+    ssl.ca_certificates = "/etc/pki/tls/cert.pem"
 
     HTTP::Client.post(@uri,
       body: body.to_json,
